59e45ebd2c75e496effa31f797cfd640b8cd4807
sunprinces <sunprince12014@gmail.com>
2017-02-28 16:08:18 +0800

    reorganize the code- better val. acc 59%

--- src/model.py
+++ src/model.py
@@ -1,35 +1,81 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from keras.models import Sequential
 from keras.layers import Input,Dense,Dropout,Flatten,Activation
 from keras.layers import Convolution2D,MaxPooling2D
-from keras.optimizers import SGD
+from keras.layers.advanced_activations import PReLU
+from keras.layers.convolutional import ZeroPadding2D,AveragePooling2D
+from keras.optimizers import SGD, Adadelta, Adam
+from termcolor import colored, cprint
 nb_class = 7
 
-def build_model():
-    # VGG-like convnet:
+def build_model(mode):
+    
+    print(colored("Use model {}".format(mode),'yellow',attrs=['bold']))
     model = Sequential()
-    model.add(Convolution2D(16,3,3,border_mode='valid',input_shape=(48,48,1)))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(16,3,3))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    if mode == 'simple':
+        # VGG-like convnet:
+        model.add(Convolution2D(16,3,3,border_mode='valid',input_shape=(48,48,1)))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(16,3,3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
-    model.add(Convolution2D(32, 3, 3, border_mode='valid'))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(32, 3, 3))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+        model.add(Convolution2D(32, 3, 3, border_mode='valid'))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(32, 3, 3))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
-    model.add(Flatten())
-    # Note: Keras does automatic shape inference.
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(nb_class))
-    model.add(Activation('softmax'))
+        model.add(Flatten())
+        # Note: Keras does automatic shape inference.
+        model.add(Dense(128))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(nb_class))
+        model.add(Activation('softmax'))
+        opt = Adam(lr=0.01,beta_1=0.9,beta_2=0.999,epsilon=1e-08,decay=0.0)
+        # opt = Adadelta(lr=0.1, rho=0.95, epsilon=1e-08)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=opt,
+                      metrics=['accuracy'])
+    elif mode == 'NTUEE':
+        model.add(Convolution2D(64,5,5,border_mode='valid', input_shape=(48,48,1)))
+        model.add(PReLU(init='zero',weights=None))
+        model.add(ZeroPadding2D(padding=(2,2),dim_ordering='tf'))
+        model.add(MaxPooling2D(pool_size=(5,5),strides=(2,2)))
 
+        model.add(ZeroPadding2D(padding=(1,1),dim_ordering='tf'))
+        model.add(Convolution2D(64,3,3))
+        model.add(PReLU(init='zero',weights=None))
+        model.add(AveragePooling2D(pool_size=(3,3),strides=(2,2)))
+        model.add(ZeroPadding2D(padding=(1,1),dim_ordering='tf'))
+        model.add(Convolution2D(128,3,3))
+        model.add(PReLU(init='zero',weights=None))
+        model.add(ZeroPadding2D(padding=(1,1),dim_ordering='tf'))
+        model.add(Convolution2D(128,3,3))
+        model.add(PReLU(init='zero',weights=None))
+        model.add(ZeroPadding2D(padding=(1,1),dim_ordering='tf'))
+        model.add(AveragePooling2D(pool_size=(3,3),strides=(2,2)))
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(PReLU(init='zero',weights=None))
+        model.add(Dropout(0.2))
+        model.add(Dense(1024))
+        model.add(PReLU(init='zero', weights=None))
+        model.add(Dropout(0.2))
+        model.add(Dense(nb_class))
+        model.add(Activation('softmax'))
+
+        opt= Adadelta(lr=0.1, rho=0.95, epsilon=1e-08)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=opt,
+                      metrics=['accuracy'])
+    model.summary()
     return model
+
--- src/train.py
+++ src/train.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from keras.optimizers import SGD,Adam,Adagrad
+import argparse
 from utils import *
 import model
 
 def main():
-    emotion_classifier = model.build_model()
-    # opt = SGD(lr=0.1,decay=1e-6,momentum=0.9,nesterov=True)
-    opt = Adam(lr=0.01,beta_1=0.9,beta_2=0.999,epsilon=1e-08,decay=0.0)
+    parser = argparse.ArgumentParser(prog='train.py',
+            description='FER2013 training script.')
+    parser.add_argument('--model',type=str,default='simple',choices=['simple','NTUEE'],
+            metavar='<model>')
+    parser.add_argument('--epoch',type=int,metavar='<#epoch>',default=20)
+    parser.add_argument('--batch',type=int,metavar='<batch_size>',default=64)
+    args = parser.parse_args()
+
+    emotion_classifier = model.build_model(args.model)
     tr_feats,tr_labels,_ = read_dataset('train')
     dev_feats,dev_labels,_ = read_dataset('publicTest')
-    emotion_classifier.compile(loss='categorical_crossentropy', optimizer=opt,metrics=['accuracy'])
-    emotion_classifier.fit(x = tr_feats,y = tr_labels,batch_size=64,nb_epoch=5,validation_data=(dev_feats,dev_labels))
-
-    f,l,_ = read_dataset('publicTest')
-    print(emotion_classifier.evaluate(f,l,batch_size=64))
-    f,l,_ = read_dataset('privateTest')
-    print(emotion_classifier.evaluate(f,l,batch_size=64))
-
+    emotion_classifier.fit(x = tr_feats,y = tr_labels,
+            batch_size=args.batch,nb_epoch=args.epoch,validation_data=(dev_feats,dev_labels))
 
 if __name__ == "__main__":
     main()
